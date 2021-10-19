38915b045ac5bd66259c155ade1bba4622c5e071
phassignments <md.saifur.rahman90@gmail.com>
2021-10-19 20:50:32 +0600

    Final Upload

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Web App's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBNYhODLT2MNs1TRsN0zCZ40EhgCK8Vxrg",
+  authDomain: "healthcare-related-websi-34875.firebaseapp.com",
+  projectId: "healthcare-related-websi-34875",
+  storageBucket: "healthcare-related-websi-34875.appspot.com",
+  messagingSenderId: "762758415720",
+  appId: "1:762758415720:web:8ee8a00340ed265bca02dd"
+};
+
+export default firebaseConfig;
