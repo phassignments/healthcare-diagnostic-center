49f7f0c62af1663170cad5683724f95d09e3bec5
phassignments <87035417+phassignments@users.noreply.github.com>
2021-10-20 16:28:33 +0600

    Add files via upload

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,58 +1,72 @@
-import React from 'react';
-import { Carousel } from 'react-bootstrap';
-import './Banner.css';
-import slideOne from '../../../Images/carousel/slide-01.jpg';
-import slideTwo from '../../../Images/carousel/slide-02.jpg';
-import slideThree from '../../../Images/carousel/slide-03.jpg';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import "./Banner.css";
+import slideOne from "../../../Images/carousel/slide-01.jpg";
+import slideTwo from "../../../Images/carousel/slide-02.jpg";
+import slideThree from "../../../Images/carousel/slide-03.jpg";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
-    return (
-        <>
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={slideOne}
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>International Quality</h3>
-                        <p>Medicit is one of the best kinds of company
-                            in the diagnostics industry in whole world.</p>
-                        <Link className='slide-btn' to='/contact'>Contact us</Link>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={slideTwo}
-                        alt="Second slide"
-                    />
+  return (
+    <>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 h-50"
+            src={slideOne}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Quality Services</h3>
+            <p>
+              Quality Health Services is all about providing the best services
+              to adults, seniors, children and disabled. We are a dependable and
+              trusted agency of choice.
+            </p>
+            <Link className="slide-btn" to="/contact">
+              Contact us
+            </Link>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 h-50"
+            src={slideTwo}
+            alt="Second slide"
+          />
 
-                    <Carousel.Caption>
-                        <h3>Best health care</h3>
-                        <p>We have the best of the best specialist doctors
-                            and therapists.</p>
-                        <Link className='slide-btn' to='/contact'>Contact us</Link>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={slideThree}
-                        alt="Third slide"
-                    />
+          <Carousel.Caption>
+            <h3>Award Winning Health System</h3>
+            <p>
+              It is one of the award-winning health system with nearly 5k
+              employees dedicated to providing the highest quality service.
+            </p>
+            <Link className="slide-btn" to="/contact">
+              Contact us
+            </Link>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 h-50"
+            src={slideThree}
+            alt="Third slide"
+          />
 
-                    <Carousel.Caption>
-                        <h3>Advanced technology</h3>
-                        <p>We have the best techonology in the medical history.</p>
-                        <Link className='slide-btn' to='/contact'>Contact us</Link>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-        </>
-    );
+          <Carousel.Caption>
+            <h3>Driving Innovation In Medtech</h3>
+            <p>
+              The focus is on patient centric. The coverage includes digital
+              transformation of the medtech, AI in surgical interventions.
+            </p>
+            <Link className="slide-btn" to="/contact">
+              Contact us
+            </Link>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
 };
 
 export default Banner;
