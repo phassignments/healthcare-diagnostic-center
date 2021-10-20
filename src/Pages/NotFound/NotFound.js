676a66f28be9e1351d91156b20aa296e10ccdfb2
phassignments <87035417+phassignments@users.noreply.github.com>
2021-10-20 16:32:51 +0600

    Add files via upload

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './NotFound.css';
+import React from "react";
+import "./NotFound.css";
 
 const NotFound = () => {
-    return (
-        <div>
-            <h2>404 NOT FOUND</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>404 PAGE NOT FOUND</h2>
+    </div>
+  );
 };
 
 export default NotFound;
